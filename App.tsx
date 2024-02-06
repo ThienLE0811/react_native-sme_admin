ec6e6a68550d4ad67fd6eb56bfe6018b81a45676
Thien Le <lethien08112001@gmail.com>
2024-02-06 10:55:28 +0700

    add font

--- App.tsx
+++ App.tsx
@@ -11,9 +11,13 @@ import {SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 function App(): React.JSX.Element {
   const [isShowSplash, setIsShowPlash] = useState<boolean>(true);
+  const {getItem} = useAsyncStorage('accessToken');
+  const [accessToken, setAccessToken] = useState<string>('');
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -22,6 +26,15 @@ function App(): React.JSX.Element {
 
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await getItem();
+    token && setAccessToken(token);
+  };
 
   return (
     <>
@@ -34,7 +47,7 @@ function App(): React.JSX.Element {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       )}
     </>
